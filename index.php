2e36432214d424f6ad20e73d6932778121028849
dunzolo <rossidavide108@gmail.com>
2023-01-27 15:39:29 +0100

    added style

--- index.php
+++ index.php
@@ -11,13 +11,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <form action="./show.php" method="POST">
-        <input type="text" name="paragraph" id="paragraph" placeholder="Inserisci il contenuto">
-        <input type="text" name="content-sub" id="content-sub" placeholder="Inserisci la parola da nascondere">
-        <button name="submit">Invia</button>
-    </form>
+    <h1 class="text-center text-danger">PHP Badwords</h1>
+    <div class="container">
+        <form action="./show.php" method="POST">
+            <div class="input-group mb-3 height-250">
+                <span class="input-group-text width-150">Paragrafo</span>
+                <textarea class="form-control" name="paragraph" id="paragraph" placeholder="Inserisci il contenuto"></textarea>
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text width-150" id="basic-addon1">Parola</span>
+                <input type="text" class="form-control" name="content-sub" id="content-sub" placeholder="Inserisci la parola da nascondere">
+            </div>
+            <div>
+                <button name="submit" class="btn btn-primary">Invia</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- show.php
+++ show.php
@@ -12,22 +12,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1>PARAGRAFO ORIGINALE</h1>
-    <h2>
-        Testo: <?php echo $paragraph ?>   
-    </h2>
-    <h2>
-        Lunghezza: <?php echo strlen ($paragraph); ?> caratteri
-    </h2>
-    <h1>PARAGRAFO SOSTITUITO</h1>
-    <h2>
-        Testo: <?php echo $new_string ?>   
-    </h2>
-    <h2>
-        Lunghezza: <?php echo strlen ($new_string); ?> caratteri
-    </h2>
+    <h1 class="text-center text-danger">PHP Badwords</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-5">
+                <h1 class="form-control text-center fs-3 bg-warning">PARAGRAFO ORIGINALE</h1>
+                <p class=form-control>
+                    <span class="fw-bold text-decoration-underline">Testo:</span> <?php echo $paragraph ?>   
+                </p>
+                <p class="form-control">
+                    <span class="fw-bold text-decoration-underline">Lunghezza:</span> <?php echo strlen ($paragraph); ?> caratteri
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="form-control text-center fs-3 bg-warning">PARAGRAFO SOSTITUITO</h1>
+                <p class=form-control>
+                    <span class="fw-bold text-decoration-underline">Testo:</span> <?php echo $new_string ?>    
+                </p>
+                <p class="form-control">
+                    <span class="fw-bold text-decoration-underline">Lunghezza:</span> <?php echo strlen ($new_string); ?> caratteri
+                </p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
